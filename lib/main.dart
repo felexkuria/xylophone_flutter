86cc70dae06c130b6bafb83db4aeda9abe66b5b8
felex <engineerfelex@gmail.com>
2020-10-31 11:12:27 +0300

    Modified  Scafold and added a flatbutton

--- lib/main.dart
+++ lib/main.dart
@@ -19,39 +19,79 @@ class XylophoneApp extends StatefulWidget {
 }
 
 class _XylophoneAppState extends State<XylophoneApp> {
-
-  void _playSound(int sound1){
-
-    final player =AudioCache();
+  void _playSound(int sound1) {
+    final player = AudioCache();
     player.play('note$sound1.wav');
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          Container(
-          color:Colors.red[800],
-        ),
-          Container(
-            color:Colors.orange,
+          Expanded(
+            child: FlatButton(
+              color: Colors.red[800],
+              onPressed: () {
+                _playSound(1);
+              },
+              child: null,
+            ),
           ),
-          Container(
-            color:Colors.yellow,
+          Expanded(
+            child: FlatButton(
+              color: Colors.orange,
+              child: null,
+              onPressed: () {
+                _playSound(2);
+              },
+            ),
           ),
-          Container(
-            color:Colors.green[400],
+          Expanded(
+            child: FlatButton(
+              color: Colors.yellow,
+              onPressed: () {
+                _playSound(3);
+              },
+              child: null,
+            ),
           ),
-          Container(
-            color:Colors.teal[400],
-          ),  Container(
-            color:Colors.blue[400],
+          Expanded(
+            child: FlatButton(
+              color: Colors.green[400],
+              onPressed: () {
+                _playSound(4);
+              },
+              child: null,
+            ),
           ),
-          Container(
-            color:Colors.purple[300],
+          Expanded(
+            child: FlatButton(
+              color: Colors.teal[400],
+              onPressed: () {
+                _playSound(5);
+              },
+              child: null,
+            ),
           ),
-
-
+          Expanded(
+            child: FlatButton(
+              color: Colors.blue[400],
+              onPressed: () {
+                _playSound(6);
+              },
+              child: null,
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              color: Colors.purple[300],
+              onPressed: () {
+                _playSound(7);
+              },
+              child: null,
+            ),
+          ),
         ],
       ),
     );
