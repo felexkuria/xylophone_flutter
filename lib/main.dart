b092fdad769c978206880e48e8131efa6aa03fbe
felex <engineerfelex@gmail.com>
2020-10-31 13:01:55 +0300

    Final commit Finished the xylophone app topics Learnt 1.Function 2.adding argument to function 3.Understanding parameters and argument in great details they 3 types of functions simple eg calculate() ,calculate(tax),and other which have return types to return data

--- lib/main.dart
+++ lib/main.dart
@@ -13,87 +13,47 @@ void main() {
   );
 }
 
-class XylophoneApp extends StatefulWidget {
-  @override
-  _XylophoneAppState createState() => _XylophoneAppState();
-}
-
-class _XylophoneAppState extends State<XylophoneApp> {
+class XylophoneApp extends StatelessWidget {
   void _playSound(int sound1) {
     final player = AudioCache();
     player.play('note$sound1.wav');
+  }
+
+  Expanded _buildKey({int soundNumber, Color color}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          _playSound(soundNumber);
+        },
+        child: null,
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Expanded(
-            child: FlatButton(
-              color: Colors.red[800],
-              onPressed: () {
-                _playSound(1);
-              },
-              child: null,
-            ),
-          ),
-          Expanded(
-            child: FlatButton(
-              color: Colors.orange,
-              child: null,
-              onPressed: () {
-                _playSound(2);
-              },
-            ),
-          ),
-          Expanded(
-            child: FlatButton(
-              color: Colors.yellow,
-              onPressed: () {
-                _playSound(3);
-              },
-              child: null,
-            ),
-          ),
-          Expanded(
-            child: FlatButton(
-              color: Colors.green[400],
-              onPressed: () {
-                _playSound(4);
-              },
-              child: null,
-            ),
-          ),
-          Expanded(
-            child: FlatButton(
-              color: Colors.teal[400],
-              onPressed: () {
-                _playSound(5);
-              },
-              child: null,
-            ),
-          ),
-          Expanded(
-            child: FlatButton(
-              color: Colors.blue[400],
-              onPressed: () {
-                _playSound(6);
-              },
-              child: null,
-            ),
-          ),
-          Expanded(
-            child: FlatButton(
-              color: Colors.purple[300],
-              onPressed: () {
-                _playSound(7);
-              },
-              child: null,
-            ),
-          ),
+          _buildKey(soundNumber: 1, color: Colors.red),
+          _buildKey(soundNumber: 2, color: Colors.orange),
+          _buildKey(soundNumber: 3, color: Colors.yellow),
+          _buildKey(soundNumber: 4, color: Colors.green),
+          _buildKey(soundNumber: 5, color: Colors.teal),
+          _buildKey(soundNumber: 6, color: Colors.blue),
+          _buildKey(soundNumber: 7, color: Colors.purple),
         ],
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
